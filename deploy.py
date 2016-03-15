356cb537309aaa39a78cfafeef723f063c78a641
Craig M. Stimmel <cstimmel@uw.edu>
2016-03-15 11:35:20 -0700

    Add beginning of a pure python script to handle deploy steps.

---
+++ deploy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+""" deploy.py- a harness for the ec2_provision playbook. This will probably
+    become a django management command eventually.
+"""
+
+def main():
+    print "Hello world."
+
+if __name__ == "__main__":
+    main()
