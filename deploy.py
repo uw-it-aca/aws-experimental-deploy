ffd56fb2ada0d1ca3b4d803fa34f1b0f75f30b26
Craig M. Stimmel <cstimmel@uw.edu>
2016-03-15 11:57:28 -0700

    Build path to the playbook to run.

--- deploy.py
+++ deploy.py
@@ -8,7 +8,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
-    print BASE_DIR
+    deploy_ec2 = os.path.join(BASE_DIR, 'aws', 'provision-ec2.yml')
+    print deploy_ec2
 
 if __name__ == "__main__":
     main()
