d602b5bae487bdca7d11af88afc1f848822c6dd0
Craig M. Stimmel <cstimmel@uw.edu>
2016-03-15 11:54:52 -0700

    Get and print the base directory.

--- deploy.py
+++ deploy.py
@@ -3,9 +3,12 @@
 """ deploy.py- a harness for the ec2_provision playbook. This will probably
     become a django management command eventually.
 """
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
-    print "Hello world."
+    print BASE_DIR
 
 if __name__ == "__main__":
     main()
