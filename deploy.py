a6f229fd5bb9c734b41a36cf273b062952cfef53
Craig M. Stimmel <cstimmel@uw.edu>
2016-04-13 17:23:47 -0700

    Refactor run_playbook to work with ansible > 2.0

--- deploy.py
+++ deploy.py
@@ -7,10 +7,16 @@
 """
 # DO NOT change the order of these imports, there's a circular dependency in
 # ansible 1.9 that will cause things to break.
-from ansible.playbook import PlayBook
+from ansible.playbook import Playbook
 from ansible.inventory import Inventory
-from ansible import callbacks
+try:
+    from ansible import callbacks
+except:
+    pass
 from ansible import utils
+
+# v2
+from runner import Runner
 
 from boto import ec2
 
@@ -28,7 +34,7 @@ def run_playbook(playbook_path, inventory_path, role):
     stats = callbacks.AggregateStats()
     runner_cb = callbacks.PlaybookRunnerCallbacks(stats, verbose=utils.VERBOSITY)
 
-    deploy_ec2 = PlayBook(
+    deploy_ec2 = Playbook(
         playbook = playbook_path,
         host_list=inventory_path,
         callbacks=playbook_cb,
@@ -37,6 +43,23 @@ def run_playbook(playbook_path, inventory_path, role):
         extra_vars = {'type': role}
     )
     results = deploy_ec2.run()
+
+def v2_run_playbook(hostnames, connection, playbook_path, inventory_path, role, private_key_file=None):
+    run_data = {
+        'type': role
+    }
+
+    runner = Runner(
+        connection=connection,
+        private_key_file=private_key_file,
+        hostnames=hostnames,
+        playbook=playbook_path,
+        run_data=run_data,
+        verbosity=3,
+    )
+
+    stats = runner.run()
+
 
 def inventory_for_project(args):
     ec2conn = ec2.connect_to_region(settings.AWS_REGION,
@@ -60,8 +83,9 @@ if __name__ == "__main__":
 
     playbook_path = os.path.join(BASE_DIR, 'aca-aws', 'provision-ec2.yml')
     inventory_path = os.path.join(BASE_DIR, 'aca-aws', 'hosts', 'localhost')
+    host = 'localhost'  # This doesn't take a list yet
     role = 'appservers'
-    run_playbook(playbook_path, inventory_path, role)
+    v2_run_playbook(host, 'local', playbook_path, inventory_path, role)
 
     playbook_path = os.path.join(BASE_DIR, 'aca-aws', 'simple.yml')
     inv = inventory_for_project(args)
