0797633cb072586064baa4f4c0695a9676ef9ab9
Patrick Michaud <pmichaud@uw.edu>
2016-04-26 17:07:38 -0700

    Adding rollback to the previous ami

---
+++ rollback.py
@@ -0,0 +1,13 @@
+from util import set_ansible_display
+display = set_ansible_display()
+# Eep
+from deploy import v2_run_playbook, BASE_DIR
+import os
+
+
+if __name__ == "__main__":
+    playbook_path = os.path.join(BASE_DIR, 'aca-aws', 'do-rollback.yml')
+    inventory_path = os.path.join(BASE_DIR, 'aca-aws', 'hosts', 'localhost')
+    role = 'infrastructure'
+
+    v2_run_playbook('localhost', 'local', playbook_path, inventory_path, role, data={"host_count": 2})
